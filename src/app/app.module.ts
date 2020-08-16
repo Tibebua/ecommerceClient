d17c633f55c75ed08d8204189bb1744ed06d3a13
Alazar <tibebualazar@gmail.com>
2020-08-17 00:27:23 +0300

    end of section 9 pagination&UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
+import { SharedModule } from './shared/shared.module';
+import { ShopModule } from './shop/shop.module';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http'
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
